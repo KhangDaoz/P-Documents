3dfef128472c26581a2343d5f81f38942d40fdab
KhangDaoz <khangda01808@gmail.com>
2026-05-23 23:38:01 +0700

    init

---
+++ src/main/java/com/ptit/p/documents/PDocuments.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ptit.p.documents;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class PDocuments {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

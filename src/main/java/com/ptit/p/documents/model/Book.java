cee3b0a4555529cfffbc396c6cdf47f077c15dff
KhangDaoz <khangda01808@gmail.com>
2026-05-28 17:05:34 +0700

    refactor: enhance Book model and add User and BookItem classes; remove BookFrm

--- src/main/java/com/ptit/p/documents/model/Book.java
+++ src/main/java/com/ptit/p/documents/model/Book.java
@@ -1,25 +1,128 @@
 package com.ptit.p.documents.model;
 
 public class Book {
-    private String id;
+    private String ISBN;
     private String title;
     private String author;
+    private String genre;
+    private String publisher;
+    private int publishYear;
+    private double price;
+    private String description;
+    private int availableCopies;
+    private int totalCopies;
 
-    public Book(String id, String title, String author) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
+    public Book() {
     }
 
-    public String getId() {
-        return id;
+    public Book(String ISBN, String title, String author, String genre,
+                String publisher, int publishYear, double price,
+                String description, int availableCopies, int totalCopies) {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.publisher = publisher;
+        this.publishYear = publishYear;
+        this.price = price;
+        this.description = description;
+        this.availableCopies = availableCopies;
+        this.totalCopies = totalCopies;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
     }
 
     public String getTitle() {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getAuthor() {
         return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getPublishYear() {
+        return publishYear;
+    }
+
+    public void setPublishYear(int publishYear) {
+        this.publishYear = publishYear;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setAvailableCopies(int availableCopies) {
+        this.availableCopies = availableCopies;
+    }
+
+    public int getTotalCopies() {
+        return totalCopies;
+    }
+
+    public void setTotalCopies(int totalCopies) {
+        this.totalCopies = totalCopies;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "ISBN='" + ISBN + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", publishYear=" + publishYear +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                ", availableCopies=" + availableCopies +
+                ", totalCopies=" + totalCopies +
+                '}';
     }
 }
